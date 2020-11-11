8d3d20c252497d88086a6a731eada30b6c58f33b
fafagb <fafagb2009@aliyun.com>
2020-11-11 18:17:07 +0800

    upload files

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # online_business
+
+# 入口index.html
+# index.html根据build和config的操作：index页面使用main.js
---
+++ online_business_ui/src/components/ProductList.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="product-search">
+      <input type="text" id="txtSearch" />
+      <button>搜索</button>
+    </div>
+    <hr />
+  </div>
+</template>
+<style >
+
+* {
+  padding: 0;
+  margin: 0;
+}
+.product-search {
+  
+  width: 704px;
+  height: 44px;
+  margin: auto;
+   
+}
+#txtSearch {
+  width: 600px;
+  height: 32px;
+  border: 6px solid red;
+  float: left;
+  outline: none;
+  padding: 0 6px;
+}
+
+button {
+  width: 80px;
+  height: 44px;
+  border: 0 none;
+  background: red;
+  color: #fff;
+  float: left;
+  outline: none;
+}
+hr {
+  margin-top: 60px;
+  border: 2px solid red;
+}
+</style>
