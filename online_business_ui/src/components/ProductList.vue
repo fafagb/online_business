00ecedceb60cdc7285b35faf6aa72a1acb485bb1
fafagb <fafagb2009@aliyun.com>
2020-11-13 18:11:27 +0800

    update files

--- online_business_ui/src/components/ProductList.vue
+++ online_business_ui/src/components/ProductList.vue
@@ -5,20 +5,51 @@
       <button>搜索</button>
     </div>
     <hr />
+    <div class="product-list">
+      <ul>
+        <!-- 属性前面加：就可以直接使用vue变量 -->
+        <li v-for="product in productList" :key="product.id"> 
+          <img :src="product.url" alt="" />
+          <p class="p-price">&yen;{{product.price}}</p>
+          <p>{{product.productName}}</p>
+        </li>
+        
+      </ul>
+    </div>
   </div>
 </template>
-<style >
+<script>
+export default {
+  data(){
 
+    return{
+
+      productlist:[]
+    };
+  },
+  mounted(){},
+  methods:{
+    getProductList:function(){
+      var thisView=this.productlist;
+      this.$http.get("/api/Products/GetProductList").then(function(res){
+
+        thisView=res.data;//如果用jequrey的话：1res就是返回的数据。2这里this是jequrey对象，但我们这里用的是axios，这里的this是个null，所以要在外面接受productlist变量，不能直接在这里this.productlist-res.data
+      });
+
+    }
+
+  }
+}
+</script>
+<style >
 * {
   padding: 0;
   margin: 0;
 }
 .product-search {
-  
   width: 704px;
   height: 44px;
   margin: auto;
-   
 }
 #txtSearch {
   width: 600px;
@@ -41,5 +72,30 @@ button {
 hr {
   margin-top: 60px;
   border: 2px solid red;
+}
+.product-list li {
+  width: 260px;
+  margin:60px 0 0 60px;
+  list-style: none;
+  float: left;
+  border: 1px solid #fff;
+}
+.product-list li:hover{
+border-color: #eee;
+box-shadow: 0 0 6px #ccc;
+
+}
+.product-list li img {
+  width: 260px;
+}
+.product-list li p {
+  text-align: left;
+}
+
+.p-price {
+  font-size: 18px;
+  font-weight: bold;
+  color: red;
+  margin-bottom: 8px;
 }
 </style>
